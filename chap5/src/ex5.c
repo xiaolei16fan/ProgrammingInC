037bb0a2b66cf7157b0b52abbb2f304635ba6766
小磊 <ranwuer@gmail.com>
2017-12-19 20:31:27 +0800

    增加第5题

--- chap5/src/ex5.c
+++ chap5/src/ex5.c
@@ -1,20 +1,33 @@
 // Program to reverse the digits of a number
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
     int number, right_digit;
+    bool isNegtive = false;
 
     printf("Enter your number.\n");
     scanf("%i", &number);
 
-    do {
-    
-        right_digit = number % 10;
-        printf("%i", right_digit);
-        number = number / 10;
+    if (number < 0)
+    {
+        number *= -1;
+        isNegtive = true;
+        do 
+        {
+            right_digit = number % 10;
+            printf("%i", right_digit);
+            number = number / 10;
+        }
+        while (number != 0);
+
+        if (isNegtive)
+        {
+            printf("-");
+        }
     }
-    while (number != 0);
+    
 
     printf("\n");
     
