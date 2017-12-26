de9a8685d9684a9a848e1723bf18b20d1bbb7e5d
Larry Chen <ranwuer@gmail.com>
2017-12-26 11:35:08 +0800

    add basic ex5.c

---
+++ basic/src/ex5.c
@@ -0,0 +1,13 @@
+// 求周长和面积
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 7;
+    int width = 5;
+
+    printf("Perimeter of the rectangle = %i inches\n", (height + width) * 2);
+    printf("Area of the rectangle = %i square inches\n", height * width);
+
+    return 0;
+}
