9880bafdabe258952e947922f0c9df47c595ab45
小磊 <ranwuer@gmail.com>
2017-12-28 20:42:14 +0800

    review basic ex15.c

--- basic/src/ex15.c
+++ basic/src/ex15.c
@@ -1,5 +1,6 @@
 // Write a C program to calculate the distance between the two points.
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -7,18 +8,18 @@ int main(void)
     float distance;
 
     printf("Input x1: ");
-    scanf("%i", x1);
+    scanf("%i", &x1);
 
     printf("Input y1: ");
-    scanf("%i", y1);
+    scanf("%i", &y1);
 
     printf("Input x2: ");
-    scanf("%i", x2);
+    scanf("%i", &x2);
 
     printf("Input y2: ");
-    scanf("%i", y2);
+    scanf("%i", &y2);
 
-    distance = 0; //TODO: 计算两点之间到距离sqrt()
+    distance = sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 
     printf("Distance between the said points: %.4f\n", distance);
 
